4b677d11ad62e7567507ee10b4a3e9482702e8c5
friday <friday@bogon>
2019-12-20 11:35:16 +0800

    IU-2019.2.4 <friday@bogon Create baseRefactoring.xml, alibaba-toolkit-tips.xml, ide.general.xml, project.default.xml, diff.xml, Enum.java, Interface.java, lombok-plugin.xml, user.xml, yiiguxing.translation.xml, Default.xml, generic-key-value-store.xml, ignore.xml, HTML File.html, find.xml, ui.lnf.xml, filetypes.xml, remote-servers.xml, colors.scheme.xml, path.macros.xml, alibaba-toolkit-settings.xml, mavenVersion.xml, templates.xml, AnnotationType.java, javarebel.xml, Default for macOS copy.xml, docker.xml, Class.java, jdk.table.xml, laf.xml, vcs.xml, editor.codeinsight.xml, keymap.xml, mybatis.xml, iterations.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * <p>
+ * ${END}
+ * </p>
+ *
+ * @author liutf
+ * @since ${DATE}
+ */
+public enum ${NAME} {
+}
